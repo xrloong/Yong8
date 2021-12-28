96a63cfc24be85b1a5f03a6c4aa26ca631bff274
Xanadu Wang <xanaduw@synology.com>
2021-12-28 12:46:05 +0800

    Refactorying. Re-org file hierarchy. Move yong8 into/

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-PYTHONPATH="solvers/gekko" python3 -m unittest discover -t . -s tests/test/ -p "test_*.py"
+PYTHONPATH="src:solvers/gekko" python3 -m unittest discover -t . -s tests/test/ -p "test_*.py"
 
